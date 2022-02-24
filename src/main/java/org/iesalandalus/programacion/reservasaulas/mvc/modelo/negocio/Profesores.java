3282974cbd3558d0e3ae6554e8f489a8149981c6
Alejandrico <Alejandrico@DESKTOP-DUKH0VE>
2022-02-24 19:58:47 +0100

    Corrijo fallos de la aplicación.

--- src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/negocio/Profesores.java
+++ src/main/java/org/iesalandalus/programacion/reservasaulas/mvc/modelo/negocio/Profesores.java
@@ -12,63 +12,64 @@ public class Profesores {
 	List<Profesor> coleccionProfesores;
 
 	public Profesores() {
-		
+
 		coleccionProfesores = new ArrayList<>();
 	}
-	
+
 	public Profesores(Profesores profesores) {
 
 		setProfesores(profesores);
 
 	}
-	
+
 	private void setProfesores(Profesores profesores) {
-		
+
 		if (profesores == null) {
 			throw new NullPointerException("ERROR: No se pueden copiar profesores nulos.");
 		}
-		
-		//CON ESTA COMPROBACIÓN VERIFICARÍAMOS QUE NO ESTABLEZCA UN LISTADO VACÍO
-		//LA COMENTO PORQUE FALLAN LOS TEST:
-		/*if (profesores.getProfesores().isEmpty()) {
-			throw new IllegalArgumentException("ERROR: La lista de profesores no puede estar vacía.");
-		}*/
-		
+
+		// CON ESTA COMPROBACIÓN VERIFICARÍAMOS QUE NO ESTABLEZCA UN LISTADO VACÍO
+		// LA COMENTO PORQUE FALLAN LOS TEST:
+		/*
+		 * if (profesores.getProfesores().isEmpty()) { throw new
+		 * IllegalArgumentException("ERROR: La lista de profesores no puede estar vacía."
+		 * ); }
+		 */
+
 		coleccionProfesores = profesores.getProfesores();
 	}
 
 	public List<Profesor> getProfesores() {
 		return copiaProfundaProfesores(coleccionProfesores);
 	}
-	
+
 	public int getNumProfesores() {
-		
+
 		return coleccionProfesores.size();
 	}
 
 	private List<Profesor> copiaProfundaProfesores(List<Profesor> profesores) {
 		List<Profesor> copiaProfesores = new ArrayList<Profesor>();
 		Iterator<Profesor> iterador = profesores.iterator();
-		
-		while(iterador.hasNext()) {
+
+		while (iterador.hasNext()) {
 			copiaProfesores.add(new Profesor(iterador.next()));
 		}
-		
+
 		return copiaProfesores;
 	}
 
 	public void insertar(Profesor insertarProfesor) throws OperationNotSupportedException {
-		
+
 		if (insertarProfesor == null) {
 			throw new NullPointerException("ERROR: No se puede insertar un profesor nulo.");
 		}
-		
+
 		if (!coleccionProfesores.contains(insertarProfesor)) {
 			coleccionProfesores.add(insertarProfesor);
 		} else {
 			throw new OperationNotSupportedException("ERROR: Ya existe un profesor con ese nombre.");
 		}
-		
 
 	}
 
@@ -76,12 +77,12 @@ public class Profesores {
 		if (buscarProfesor == null) {
 			throw new NullPointerException("ERROR: No se puede buscar un profesor nulo.");
 		}
-	
+
 		Iterator<Profesor> iterador = coleccionProfesores.iterator();
-		
-		while(iterador.hasNext()) {
+
+		while (iterador.hasNext()) {
 			Profesor profesorBuscado = iterador.next();
-			if(buscarProfesor.equals(profesorBuscado)) {
+			if (buscarProfesor.equals(profesorBuscado)) {
 				return new Profesor(profesorBuscado);
 			}
 		}
@@ -93,7 +94,7 @@ public class Profesores {
 		if (borrarProfesor == null) {
 			throw new NullPointerException("ERROR: No se puede borrar un profesor nulo.");
 		}
-		
+
 		if (coleccionProfesores.contains(borrarProfesor)) {
 			coleccionProfesores.remove(borrarProfesor);
 		} else {
